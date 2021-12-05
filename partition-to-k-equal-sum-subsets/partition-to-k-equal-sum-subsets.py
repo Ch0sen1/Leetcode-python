80b41a98245e709713ce6069a9f283b40156a4fe
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2021-12-04 17:48:31 -0800

    Time: 55 ms (61.02%), Space: 14.2 MB (78.10%) - LeetHub

--- partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
+++ partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -1,28 +1,29 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        nums_sum = sum(nums)
-        if nums_sum % k != 0:
+        totalSum = sum(nums)
+        if totalSum % k != 0:
             return False
-        subset_sum = nums_sum / k
-        ks = [0] * k
-        nums.sort(reverse=True)
+        targetSum = totalSum / k
         visited = [False] * len(nums)
-        return self.dfs(nums, visited, 0, k, 0, subset_sum)
+        return self.dfs(nums, visited, targetSum, 0, k, 0)
         
-    def dfs(self, nums, visited, start, k, curSum, targetSum):
+    
+    def dfs(self, nums, visited, targetSum, curSum, k, start):
         if k == 1:
             return True
         if curSum == targetSum:
-            return self.dfs(nums, visited, 0, k - 1, 0, targetSum)
+            return self.dfs(nums, visited, targetSum, 0, k-1, 0)
         
+            
         for i in range(start, len(nums)):
-            if not visited[i] and curSum + nums[i] <= targetSum:
+            if visited[i] != True and curSum + nums[i] <= targetSum:
                 visited[i] = True
-                if self.dfs(nums, visited, i + 1, k, curSum+nums[i], targetSum):
+                if self.dfs(nums, visited, targetSum, curSum+nums[i], k, i + 1):
                     return True
                 visited[i] = False
-        
         return False
+        
+        
         
         
         
