a892e1251931cf34dd3e04a24534c43092aec5e0
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2021-12-23 15:01:22 -0800

    Time: 32 ms (62.48%), Space: 14.2 MB (52.46%) - LeetHub

--- powx-n/powx-n.py
+++ powx-n/powx-n.py
@@ -4,13 +4,16 @@ class Solution:
             return 1 / self.helper(x, -n)
         else:
             return self.helper(x, n)
-        
-        
+    
     def helper(self, x, n):
+        if x == 0:
+            return 0
         if n == 0:
             return 1
-        half = self.helper(x, n // 2)
+        
+        half = self.helper(x, n//2)
         if n % 2 == 0:
             return half * half
         else:
             return half * half * x
+        
