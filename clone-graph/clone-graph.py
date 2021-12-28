e2d0304b60423d89696826e65e385bdbb0194608
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2021-12-28 01:03:48 -0800

    Time: 36 ms (79.55%), Space: 14.5 MB (95.19%) - LeetHub

---
+++ clone-graph/clone-graph.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+        oldToNew = {}
+        oldToNew[node] = Node(node.val)
+        self.dfs(node, oldToNew)
+        return oldToNew[node]
+        
+    
+    
+    def dfs(self, node, m):
+        for neigh in node.neighbors:
+            if neigh not in m:
+                m[neigh] = Node(neigh.val)
+                self.dfs(neigh, m)
+            m[node].neighbors.append(m[neigh])
+        
+        
+        
