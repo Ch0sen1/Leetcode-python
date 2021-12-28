e0d7ec7b180867e86ffa92717dd2bba57f12109f
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2021-12-27 20:18:50 -0800

    Time: 308 ms (62.05%), Space: 23.5 MB (49.04%) - LeetHub

---
+++ random-pick-index/random-pick-index.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.dic = defaultdict(list)
+        for i, num in enumerate(nums):
+            self.dic[num].append(i)
+        
+
+    def pick(self, target: int) -> int:
+        if target in self.dic:
+            curList = self.dic[target]
+            index = randrange(0, len(curList))
+            return curList[index]
+            
+        
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
