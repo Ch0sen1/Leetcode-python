914f506ff9b68cb5c0744ccbc3d25f48cfb5ff8e
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-01-05 17:41:45 -0800

    Time: 414 ms (27.00%), Space: 18.7 MB (87.51%) - LeetHub

---
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        
+        for i in range(1, len(w)):
+            w[i] = w[i] +w[i-1]
+            
+        self.weightSum = w
+        
+    def pickIndex(self) -> int:
+        # probability of pibkcing W[I] / sum(w)
+        idx = random.randint(1, self.weightSum[-1])
+        l = 0
+        r = len(self.weightSum) - 1
+        
+        while l < r:
+            mid = (l+r) // 2
+            if self.weightSum[mid] < idx:
+                l = mid + 1
+            else:
+                r = mid
+        return l
+        
+        
+        
+
