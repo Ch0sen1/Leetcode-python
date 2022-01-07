efa6c00d3ce92be7052cd9317a203b7bf52d07e2
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-01-07 04:01:04 -0800

    Time: 327 ms (8.23%), Space: 15.1 MB (13.66%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        # i is itself, List[i] is its neighbors
+        color = 1
+        dic = defaultdict()
+        for i in range(len(graph)):
+            if i not in dic:
+                if self.bfs(graph, i, dic) == False:
+                    return False
+        return True
+        
+        
+    def bfs(self, graph, i, dic):
+        q = deque()
+        q.append((i,1))
+        while q:
+            node, color = q.popleft()
+            if node in dic:
+                if dic[node] != color:
+                    return False
+            else:
+                dic[node] = color
+                for neigh in graph[node]:
+                    q.append((neigh, -color))
+        return True
+                
+                
+        
+        
+            
+        
+        
+        
