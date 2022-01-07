4345dfae61cfd4f508509f9100413d64e5f24b7f
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-01-07 03:30:25 -0800

    Time: 33 ms (26.81%), Space: 14.1 MB (72.48%) - LeetHub

---
+++ 266-palindrome-permutation/266-palindrome-permutation.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        c = Counter(s)
+        haveOne = 0
+        for key, value in c.items():
+            if value % 2 == 1:
+                haveOne +=1
+            if haveOne >= 2:
+                return False
+        
+        return True
+        
