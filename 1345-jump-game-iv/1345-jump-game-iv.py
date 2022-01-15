cf37310af550ed9410d8b658b7fc3f5be84457c4
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-01-14 19:50:37 -0800

    Time: 1161 ms (13.71%), Space: 31.1 MB (29.12%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        if not arr:
+            return -1
+        dic = defaultdict(list)
+        for i, num in enumerate(arr):
+            dic[num].append(i)
+            
+        visited_index = set()
+        visited_group = set()
+        
+        q = deque()
+        q.append((0,0))
+        while q:
+            index, step = q.popleft()
+            if index == len(arr) - 1:
+                return step
+            # first two type of jump
+            for neigh in [(index - 1), (index + 1)]:
+                if 0 <= neigh < len(arr) and neigh not in visited_index:
+                    q.append((neigh, step+1))
+                    visited_index.add(neigh)
+            
+            # third type of jump
+            
+            if arr[index] not in visited_group:
+                for neigh in dic[arr[index]]:
+                    if 0 <= neigh < len(arr) and neigh not in visited_index:
+                        q.append((neigh, step+1))
+                        visited_group.add(arr[index])
+                        visited_index.add(neigh)
+            
+        return -1
+            
+            
+                
+        
+        
