354ce19766f427cd22337a0a40c6a8bde9009913
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-01-23 22:57:27 -0800

    Time: 74 ms (13.78%), Space: 14.4 MB (15.75%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        
+        res = 0
+        refund = [0] * len(costs)
+        
+        for i in range(len(costs)):
+            
+            res += costs[i][0]
+            refund[i] = costs[i][1] - costs[i][0]
+        
+        n = len(costs) // 2
+        refund = sorted(refund)
+        
+        for i in range(n):
+            res += refund[i]
+            
+        return res            
+            
