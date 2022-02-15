c7a59e5c268668464ae381c5f56ae0e5b44a7aa7
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-02-15 08:22:00 -0800

    Time: 60 ms (8.58%), Space: 13.8 MB (89.20%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,10 +1,17 @@
 class Solution:
-    @lru_cache(None)
-    def climbStairs(self, n: int) -> int:
-        if n <= 1:
-            return 1
-        else:
-            return self.climbStairs(n-1) + self.climbStairs(n-2)
+    # @lru_cache(None)
+    # def climbStairs(self, n: int) -> int:
+    #     if n <= 1:
+    #         return 1
+    #     else:
+    #         return self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+        
+    def climbStairs(self, n):
+        dp = [1] * (n+1)
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
         
         
         
