f3b2e56b7b468045087a8e0cb5087334562b402f
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-02-18 00:04:50 -0800

    Time: 53 ms (58.73%), Space: 14.2 MB (81.82%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.py
+++ 402-remove-k-digits/402-remove-k-digits.py
@@ -7,8 +7,15 @@ class Solution:
                 k -= 1
             stack.append(digit)
         
-        if k > 0:
-            stack = stack[:-k]
-        
-        return ''.join(stack).lstrip("0") or '0'
+        # If no elements are removed, pop last elements, (increasing order)
+        while(k):
+            stack.pop()
+            k -= 1
+
+        # removing leading zeros
+        i = 0
+        while( i <len(stack) and stack[i] == "0" ):
+            i += 1
+            
+        return ''.join(stack[i:]) if (len(stack[i:]) > 0) else "0"       
         
