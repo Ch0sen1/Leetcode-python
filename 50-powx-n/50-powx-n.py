ab13ea278726dc8dd3ba69639b1298f27c621d56
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-02-19 14:50:06 -0800

    Time: 59 ms (13.75%), Space: 13.8 MB (99.31%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,11 +1,11 @@
 class Solution:
-    def myPow(self, x: float, n: int) :
+    def myPow(self, x: float, n: int) -> float:
         if n < 0:
-            return 1 / self.dfs(x, -n) 
+            return 1 / self.dfs(x, -n)
         else:
             return self.dfs(x, n)
-        
-        
+    
+    
     def dfs(self, x, n):
         if x == 0:
             return 0
@@ -13,9 +13,9 @@ class Solution:
             return 1
         
         half = self.dfs(x, n//2)
+        
         if n % 2 == 0:
             return half * half
         else:
             return half * half * x
-        
         
