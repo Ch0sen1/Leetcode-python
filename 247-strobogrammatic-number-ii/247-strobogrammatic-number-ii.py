d3bf21cc8638eccc245782bd08057dd8bd1caac1
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-03-14 23:21:01 -0700

    Time: 226 ms (72.69%), Space: 25.8 MB (69.84%) - LeetHub

--- 247-strobogrammatic-number-ii/247-strobogrammatic-number-ii.py
+++ 247-strobogrammatic-number-ii/247-strobogrammatic-number-ii.py
@@ -1,19 +1,27 @@
 class Solution:
-    def findStrobogrammatic(self, n):
-        evenMidCandidate = ["11","69","88","96", "00"]
-        oddMidCandidate = ["0", "1", "8"]
+    def findStrobogrammatic(self, n: int) -> List[str]:
+        self.even_mid = ['11','69','88','96','00']
+        self.odd_mid = ['0','1','8']
+        return self.dfs(n)
+        
+    def dfs(self, n):
         if n == 1:
-            return oddMidCandidate
-        if n == 2:
-            return evenMidCandidate[:-1]
-        if n % 2 == 1:
-            pre, midCandidate = self.findStrobogrammatic(n-1), oddMidCandidate
-        else: 
-            pre, midCandidate = self.findStrobogrammatic(n-2), evenMidCandidate
-        premid = (n-1)//2
+            return self.odd_mid
+        elif n == 2:
+            return self.even_mid[:-1]
+        elif n % 2 == 1:
+            pre, mid_candidate = self.dfs(n-1), self.odd_mid
+        else:
+            pre, mid_candidate = self.dfs(n-2), self.even_mid
+        
         res = []
-        for c in midCandidate:
-            for p in pre:
-                res.append(p[:premid] + c + p[premid:])
+        premid = (n-1)//2
+        
+        for p in pre:
+            for m in mid_candidate:
+                res.append(p[:premid] + m + p[premid:])
         return res
+        
+        
+            
         
