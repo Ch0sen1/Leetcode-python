d74c3bcd6a117f735dcb6a8f6bc31ee201f1e6d6
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-03-22 18:24:39 -0700

    Time: 57 ms (15.98%), Space: 13.8 MB (73.06%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.py
+++ 991-broken-calculator/991-broken-calculator.py
@@ -1,10 +1,18 @@
 class Solution:
     def brokenCalc(self, X, Y):
         res = 0
-        while X < Y:
-            res += Y % 2 + 1
-            Y = (Y + 1) // 2
-        return res + X - Y
+        while X != Y:
+            if Y < X:
+                res += (X-Y)
+                break
+            else:
+                if Y%2 == 0:
+                    Y = Y//2
+                else:
+                    Y = Y+1
+                res += 1
+        return res
+                
             
             
         
