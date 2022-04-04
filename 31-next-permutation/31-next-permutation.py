331dc864a495d9d47c032cbabd21efa99d7e78d3
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-04-04 13:21:13 -0700

    Time: 70 ms (29.71%), Space: 13.9 MB (79.22%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,31 +1,40 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        i = len(nums) - 1
-        while i > 0 and nums[i-1] >= nums[i]:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        # find first number that are greater than previous
+        
+        i = j = k = len(nums) - 1
+        
+        while i > 0 and nums[i] <= nums[i-1] :
             i -= 1
-            
+        
         if i == 0:
             nums.reverse()
             return
         
-        swap_item = i - 1
+        nums_swap = i - 1
         
-        j = len(nums) - 1
-        
-        while j > 0 and nums[j] <= nums[swap_item]:
+        while j > 0 and nums[j] <= nums[nums_swap]:
             j -= 1
             
-        nums[swap_item], nums[j] = nums[j], nums[swap_item]
+        nums[nums_swap], nums[j] = nums[j], nums[nums_swap]
         
-        k = len(nums) - 1
+        
         while i < k:
             nums[i], nums[k] = nums[k], nums[i]
             i += 1
             k -= 1
             
-        return 
-            
-
+        return
+        
+        
+        
+        
+        
+        
         
         
         
