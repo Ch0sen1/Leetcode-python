2b3c75a8a61dea46bfad7aa50b8a005f1bec425a
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-04-08 18:23:35 -0700

    Time: 223 ms (7.29%), Space: 27.6 MB (5.01%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,11 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        order = [set() for i in range(len(nums)+1)]
         c = Counter(nums)
-        order = [[] for i in range(len(nums)+1)] 
         res = []
         for key, value in c.items():
-            order[value].append(key)
-        
+            order[value].add(key)
+            
         for i in range(len(order)-1, -1, -1):
             if order[i]:
                 for ele in order[i]:
@@ -13,5 +13,8 @@ class Solution:
                     k -= 1
                     if k == 0:
                         return res
+        
+        return res
+        
             
         
