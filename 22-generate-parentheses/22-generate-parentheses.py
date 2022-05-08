5c6efea20fe24dd32c2f97947198fcf9faaf6cb3
Ch0sen1 <45779379+Ch0sen1@users.noreply.github.com>
2022-05-08 15:53:05 -0700

    Time: 46 ms (57.86%), Space: 14 MB (98.41%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -3,19 +3,17 @@ class Solution:
         res = []
         self.dfs(n, n, "", res)
         return res
-        
-        
-        
-        
-    def dfs(self, left, right, path, res):
-        
-        if left == 0 and right == 0:
+    
+    def dfs(self, right, left, path, res):
+        if right == 0 and left == 0:
             res.append(path)
             return
-        if left > right or left < 0 or right < 0:
+        if right > left or right < 0 or left < 0:
             return
-        self.dfs(left-1, right, path+'(', res)
-        self.dfs(left, right-1, path+')', res)
+        self.dfs(right - 1, left, path+ '(', res)
+        self.dfs(right, left - 1, path+ ')', res)
+        
+        
         
         
         
